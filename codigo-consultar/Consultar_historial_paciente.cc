915cebd244a2e25b7bfe6de4f537f6820df9d170
i62rurep <33512095+i62rurep@users.noreply.github.com>
2019-11-22 13:08:11 +0100

    codigos en progreso de los consultar
    codigos de 3 funciones consultar, todavía estan en progreso y no son definitivos.

---
+++ codigo-consultar/Consultar_historial_paciente.cc
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include "paciente.h"
+
+int int main() {
+
+    Consultar_historial_paciente(){
+      //Tiene que dar como minimo 1 sintoma/tratamiento
+
+      getHistorialPaciente(string Nombre, string Apellido);
+      printf("El historial completo del paciente es el siguiente: \n", );
+      scanf("%s\n", getHistorialPaciente);
+    }
+}
