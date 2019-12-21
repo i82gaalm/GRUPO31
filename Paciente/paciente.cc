338c43d948f2879549fc5a0820feb8991ec7c7b1
i82gaalm <55752079+i82gaalm@users.noreply.github.com>
2019-12-21 19:22:32 +0100

    Update paciente.cc

--- Paciente/paciente.cc
+++ Paciente/paciente.cc
@@ -1,33 +1,75 @@
 #include "paciente.h"
 //Comprobar que está en la lista de pacientes!!!!!!!!
 Paciente::Consultar_datos(string nombre,string apellidos){
-  printf("Datos: \n");
-  string nomap=getNombre()+" "+getApellidos();
-  printf("Nombre y Apellidos: %s\n", nomap);
-  printf("Código Postal: %d\n", getCodigoPostal());
-  printf("Teléfono: %d\n", getTelefono());
-  printf("Fecha de Naciminento: %s\n", getNaciminento());
-  printf("Domicilio: %s\n", getDomicilio());
-  printf("Seguro: %s\n", getSeguro());
+	int aux=1;
+	for(list<Paciente>::iterator it=pacientes_.begin() ; it!=pacientes_.end(); it++){
+    	if (it.getNombre()==nombre && it.getApellidos()==apellidos)
+    	{
+    		aux=0;
+    		printf("Datos: \n");
+			string nomap=getNombre()+" "+getApellidos();
+			printf("Nombre y Apellidos: %s\n", nomap);
+			printf("Código Postal: %d\n", getCodigoPostal());
+			printf("Teléfono: %d\n", getTelefono());
+			printf("Fecha de Naciminento: %s\n", getNaciminento());
+			printf("Domicilio: %s\n", getDomicilio());
+			printf("Seguro: %s\n", getSeguro());		
+    	}
+  	}
+  	if(aux==1){printf("El paciente introducido no se encuentra registrado en el sistema";}
 }
 
 Paciente::Consultar_lista(list<Paciente> lista){
   string nomap;
   for(list<Paciente>::iterator it=lista.begin() ; it!=lista.end(); it++){
     nomap=getNombre()+" "+getApellidos();
-    printf("|Nombre y Apellidos: %s | Teléfono: %d | Domicilio: %s | Seguro: %s |\n", nomap, getTelefono(), getDomicilio(), getSeguro();
+    printf("|Nombre y Apellidos: %s | Teléfono: %d | Domicilio: %s | Seguro: %s |\n", nomap, it.getTelefono(), it.getDomicilio(), it.getSeguro());
   }
 }
 
-Citas::Consultar_paciente(string nombre, string apellidos){
-
+Cita::Consultar_paciente(string nombre, string apellidos){
+	int aux=1;
+	for(list<Citas>::iterator it=citas_.begin() ; it!=citas_.end(); it++){
+    	if (it.getNombre()==nombre && it.getApellidos()==apellidos)
+    	{
+    		aux=0;
+    		printf("|Fecha: %s | Hora inicial: %s | Hora final: %s | Médico: %s | Consulta: %d |\n", it.getFecha(), it.getInicio(), it.getFinal(), it.getMedico(), it.getConsulta());
+    	}
+  	}
+  	if(aux==1){printf("El paciente introducido no se encuentra registrado en el sistema\n";}
 }
 
-Citas::Consultar_dia(string fecha){
-
+Cita::Consultar_dia(string fecha){
+	printf("Citas del día: \n");
+	int aux=1;
+	for(list<Citas>::iterator it=citas_.begin() ; it!=citas_.end(); it++){
+    	if (fecha==it.getFecha())
+    	{
+    		aux=0;
+    		printf("|Fecha: %s | Hora inicial: %s | Hora final: %s | Médico: %s | Consulta: %d |\n", it.getFecha(), it.getInicio(), it.getFinal(), it.getMedico(), it.getConsulta());
+    	}
+  	}
+  	if(aux==1){printf("No hay citas registradas\n";}
 }
 
-Historial_Paciente::Consultar(string nombre, string apellidos){
-
+Historial_Paciente::Consultar_historial(string nombre, string apellidos){
+	int c=1;
+	int a;
+	for(list<Historial_Paciente>::iterator it=historial_.begin() ; it!=historial_.end(); it++){
+    	if (it.getNombre()==nombre && it.getApellidos()==apellidos)
+    	{
+    		printf("Registro %d: \n", c);
+    		printf("Sintomas: %s\n", it.getSintomas());
+			printf("Diagnostico: %d\n", it.getDiagnostico());
+			for (int i = 0; i < (it->getTratamientos().size()); ++i)
+			{
+				a=i+1;
+				printf("Tratamiento %d: %s\n", a, it->getTratamientos()[i]);	
+			}
+			printf("Fecha: %s\n", it.getFecha());
+			c++;
+    	}
+  	}
+  	if(c==1){printf("El paciente introducido no se encuentra registrado en el sistema\n";}
 }
 
